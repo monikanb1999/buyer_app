02ed6c885a02fda0be88feb34a5706702a23055a
monika <monikanb1999@gmail.com>
2021-10-13 10:25:12 +0530

    person searching

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -1,50 +1,11 @@
 import 'package:flutter/material.dart';
 
 
-class FirstScreen extends StatefulWidget {
+class FirstScreen extends StatelessWidget {
   const FirstScreen({Key? key}) : super(key: key);
 
   @override
-  _FirstScreenState createState() => _FirstScreenState();
-}
-
-class _FirstScreenState extends State<FirstScreen> {
-  final List<String> _texts = [
-    "InduceSmile.com",
-    "Flutter.io",
-    "google.com",
-    "youtube.com",
-    "yahoo.com",
-    "gmail.com"
-  ];
-  late List<bool> _isChecked;
-@override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _isChecked = List<bool>.filled(_texts.length, false);
-
-}
-  @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: _texts.length,
-      itemBuilder: (context, index) {
-        return CheckboxListTile(
-          title: Text(_texts[index]),
-          value: _isChecked[index],
-          onChanged: (val) {
-            setState(
-                  () {
-                _isChecked[index] = val!;
-              },
-            );
-          },
-        );
-      },
-    );
+    return Container();
   }
 }
-
-
-
--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -1,12 +1,54 @@
 import 'package:flutter/material.dart';
 
 
-class SecondScreen extends StatelessWidget {
+class SecondScreen extends StatefulWidget {
   const SecondScreen({Key? key}) : super(key: key);
 
   @override
+  _SecondScreenState createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  final List<String> _texts = [
+    "InduceSmile.com",
+    "Flutter.io",
+    "google.com",
+    "youtube.com",
+    "yahoo.com",
+    "gmail.com"
+  ];
+  late List<bool> _isChecked;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _isChecked = List<bool>.filled(_texts.length, false);
+
+  }
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.separated(
+      itemCount: _texts.length,
+      itemBuilder: (context, index) {
+        return CheckboxListTile(
+          title: Image.asset('assets/road.jpg',width: 100.0,height: 150.0,
+            alignment: Alignment.topLeft,fit: BoxFit.contain,),
+          value: _isChecked[index],
+          onChanged: (val) {
+            setState(
+                  () {
+                _isChecked[index] = val!;
+              },
+            );
+          },
+        );
+      }, separatorBuilder: (BuildContext context, int index)
+    { return const Divider(height: 2.0,thickness: 2.0,); },
+    );
+
   }
 }
+
+
+
 
